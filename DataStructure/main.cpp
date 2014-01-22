e2b0b0585dc1ef3e9bb86168c3261b38e27e5d50
Dominic Chang <doc.dominic@gmail.com>
2014-01-21 21:04:43 -0800

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by Dominic Chang on 1/21/14.
+//  Copyright (c) 2014 Dominic Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
